b20c3395791cc3625fa78e4b620d0b599cb2fdae
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-24 04:48:37 -0700

    header file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include <sort.h>
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ *	 using the Bubble sort algorithm
+ * @array: int pointer to array of numbers
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
