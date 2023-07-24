35dbe17f2ca90cf48ed6a064f7f6d65a39de40c9
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-24 04:41:09 -0700

    header file

---
+++ sort.h
@@ -0,0 +1,7 @@
+#ifndef SORT_H
+#define SORT_H
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+#endif
