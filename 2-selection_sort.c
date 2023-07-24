9c2a2333178c83077f4ff5cc68820158a171d80b
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-24 07:45:54 -0700

    selection soert

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,32 @@
 #include"sort.h"
 /**
- * */
+ * selection_sort - sorts an array of integers
+ *	in ascending order using the Selection sort algorithm
+ * @array: pointer to array of int numbers
+ * @size: int size of array
+ * Return Void
+ */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j = 0;
+	size_t mini, temp = 0;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		mini = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[mini])
+			{
+				mini = j;
+			}
+		}
+		if (mini != i)
+		{
+			temp = array[i];
+			array[i] = array[mini];
+			array[mini] = temp;
+			print_array(array, size);
+		}
+	}
 }
